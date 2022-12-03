00cfc70f89ada298942a86e3856884e541913aaf
Fell1p <luofelipe@hotmail.com>
2022-12-02 23:04:11 -0300

    Criação do formulario

---
+++ src/comon/info.ts
@@ -0,0 +1,6 @@
+export interface Info {
+    nome: string,
+    numeroCartao: String,
+    dataValidade: string,
+    cvc: string
+}
---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Formulario(){
+    return(
+        <form>
+        <div>
+            <label htmlFor="">
+                
+            </label>
+            <input 
+                type="text" 
+                name="" 
+                id="" 
+            />
+        </div>
+        <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+        </div>
+        <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+        </div>
+        <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+        </div>
+        <div>
+            <label htmlFor=""></label>
+            <input type="text" />
+        </div>
+        </form>
+    )
+}
